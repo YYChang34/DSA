7dea13404f7b8c5d92eb0494c40dee7e524e382b
B0922044 <B0922044@cgu.edu.tw>
2022-09-16 12:18:58 +0800

    demo

---
+++ HW1_malloc.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int x, y;
+	cout << "請輸入x和y並以空格隔開" << endl;
+	cin >> x >> y;
+
+	char** c = (char**)malloc(x * sizeof(char*));
+
+	int i, j;
+	for(i = 0; i < x; i++) {
+		c[i] = (char*)malloc(y * sizeof(char));
+	}
+
+	for(i = 0; i < x; i++) {
+		cin >> c[i];
+	}
+
+	for(i = 0; i < x; i++) {
+		for(j = 0; j < x - i - 1; j++) {
+			if(**(c + j) < **(c + j + 1)) {
+				char* temp = *(c + j);
+				*(c + j) = *(c + j + 1);
+				*(c + j + 1) = temp;
+			}
+		}
+	}
+
+	for(i = 0; i < x; i++) {
+		cout << c[i] << endl;
+	}
+
+	for(i = 0; i < x; i++) {
+		free(c[i]);
+	}
+	free(c);
+
+	return 0;
+}
